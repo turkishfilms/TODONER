8a828fb7d1069e11a32479fdcbb96257ffb2cf41
Reginald Robinson <ddrbnsn@gmail.com>
2023-07-18 16:13:14 -0700

    added buttons to app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
 // import { useState } from 'react'
 
+import Button from "./Button"
+
 const App = () => {
   return (<>
   <div>Hello</div>
+  <Button>Hey</Button>
+  <Button>Noway</Button>
   </>)
 }
 
