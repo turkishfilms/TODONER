7f584a3533d16363118c16505cdfc77c81345a11
Reginald Robinson <ddrbnsn@gmail.com>
2023-07-18 17:10:20 -0700

    Added Children to button
    ---
    Decidedd they are ddifferent than navbar elements
    ---

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 interface ButtonInterface {
-children: String,
-count: Number
+  children: String;
+  count: Number;
 }
 
-const Button = ({children,count}:ButtonInterface) => {
-  return (<>
-    <div>{ children }</div>
-    <div>{ count }</div>
-</>
-  )
-}
-export default Button
+const Button = ({ children, count }: ButtonInterface) => {
+  return (
+    <>
+      <div className="btn btn-primary">{children}</div>
+      <div>heyhey+ {count}</div>
+    </>
+  );
+};
+export default Button;
