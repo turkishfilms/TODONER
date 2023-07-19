88cba0c528a9effeb49cc0c06839fae384a3d3e5
Reginald Robinson <ddrbnsn@gmail.com>
2023-07-19 15:47:47 -0700

    Added hook to handleView
    ---
    Navbar is one element right now
    Each "link" changes app state
    ---

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,13 +1,42 @@
-function TopBar() {
+interface TopBarProps {
+  onClickLink: (viewName: string) => void;
+}
+
+function TopBar({ onClickLink }: TopBarProps) {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">hey</li>
-            <li className="nav-item">Blockchain Explorer</li>
-            <li className="nav-item">donate</li>
-          </ul>
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  className="nav-link active"
+                  aria-current="page"
+                  href="#"
+                  onClick={() => onClickLink("home")}>
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={() => onClickLink("chain")}>
+                  Blockchain Explorer
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a
+                  className="nav-link"
+                  href="#"
+                  onClick={() => onClickLink("donate")}>
+                  Donate
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
