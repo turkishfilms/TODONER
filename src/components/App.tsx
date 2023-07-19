4a3568b58a02d46665529c6d8e1b1fece65c791a
Reginald Robinson <ddrbnsn@gmail.com>
2023-07-19 14:17:35 -0700

    slowgoing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,25 +4,27 @@ import TopBar from "./TopBar";
 import TodoInput from "./TodoInput";
 import { useState } from "react";
 
-const [screen, setScreen] = useState({
-  home: 1,
-  chain: 0,
-  donate:0
-});
-
-const App = () => {
+function App() {
+  const [screen, setScreen] = useState({
+    home: 1,
+    chain: 0,
+    donate: 0,
+  });
+  // setScreen({ home: 1, chain: 0, donate: 0 });
   return (
     <>
-      <TopBar></TopBar>
-      {screen.home ?? (
+      <TopBar />
+      {screen.home && (
         <div>
           <Button>New Item</Button>
-          <TodoInput></TodoInput>
-          <ListArea></ListArea>
+          <TodoInput />
+          <ListArea />
         </div>
       )}
+      {screen.chain && <div>chain</div>}
+      {screen.donate && <div>btc or avax plz</div>}
     </>
   );
-};
+}
 
 export default App;
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,29 +1,17 @@
-const TopBar = () => {
+function TopBar() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-primary">
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Blockchain Explorer
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Donate
-              </a>
-            </li>
+            <li className="nav-item active">hey</li>
+            <li className="nav-item">Blockchain Explorer</li>
+            <li className="nav-item">donate</li>
           </ul>
         </div>
       </nav>
     </>
   );
-};
+}
 
 export default TopBar;
